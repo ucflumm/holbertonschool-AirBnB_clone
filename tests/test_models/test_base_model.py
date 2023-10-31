e7cd88485dbef0b3a40c807cd14bf77f00e869be
jess6718 <6718@holbertonstudents.com>
2023-10-31 21:50:56 +0100

    feat task 3 test base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,41 @@
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Tests for the BaseModel class"""
+
+    def setUp(self):
+        """Init Basemodel"""
+        self.bm = BaseModel()
+
+    def test_init(self):
+        """Test init of BaseModel"""
+        self.assertIsInstance(self.bm, BaseModel)
+
+    def test_str(self):
+        """Test __str__ method"""
+        expected_val = (
+            f"[{self.bm.__class__.__name__}] ({self.bm.id}) "
+            f"{{'id': '{self.bm.id}', "
+            f"'created_at': '{self.bm.created_at.isoformat()}', 'updated_at': "
+            f"'{self.bm.updated_at.isoformat()}', '__class__': "
+            f"'{type(self.bm).__name__}'}}"
+        )
+        self.assertEqual(str(self.bm), expected_val)
+
+    def test_save(self):
+        """Test save method"""
+        origin_update_at = self.bm.updated_at
+        self.bm.save()
+        after_save_update_at = self.bm.updated_at
+        self.assertNotEqual(origin_update_at, after_save_update_at)
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        created_at = self.bm.created_at.isoformat()
+        updated_at = self.bm.updated_at.isoformat()
+        new_dict = self.bm.to_dict()
+        self.assertEqual(new_dict["created_at"], created_at)
+        self.assertEqual(new_dict["updated_at"], updated_at)
+        self.assertEqual(new_dict["__class__"], type(self.bm).__name__)
